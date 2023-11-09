9990f327c74b3f4377ff4b7e14efb6725625bab3
plopezgit <p.lopez.ch.hr@hotmail.com>
2023-11-09 11:09:37 +0100

    inital commit creates gradle projectdir

---
+++ src/main/java/org/gradle/n1Exe1/client/App.java
@@ -0,0 +1,15 @@
+package org.gradle.n1Exe1.client;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		/*
+			Diseña un sistema en el que un Agente de Bolsa (Observable) 
+			notifica a varias agencias de Bolsa (Observers) cambios cuando la Bolsa sube o baja.
+			Es necesario que el objeto Observable mantenga referencias a los Observadores.
+		*/	
+		
+	}
+	
+}
