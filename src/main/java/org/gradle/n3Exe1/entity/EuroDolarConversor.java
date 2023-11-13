9edf0495bb2644829d54d83643b6a9ce595ad3cc
plopezgit <p.lopez.ch.hr@hotmail.com>
2023-11-13 22:38:43 +0100

    refactors CurrencyConverter implementations

--- src/main/java/org/gradle/n3Exe1/entity/EuroDolarConversor.java
+++ src/main/java/org/gradle/n3Exe1/entity/EuroDolarConversor.java
@@ -3,15 +3,6 @@ package org.gradle.n3Exe1.entity;
 public class EuroDolarConversor implements CurrencyConverter {
 	
 	private final float EXCHANGE_RATE = 1.07F;
-	private final String CURRENCY = "USD";
-	
-	public float getEXCHANGE_RATE() {
-		return EXCHANGE_RATE;
-	}
-
-	public String getCURRENCY() {
-		return CURRENCY;
-	}
 	
 	@Override
 	public float conversionFrom(float amount) {
--- src/main/java/org/gradle/n3Exe1/entity/EuroYuanConversor.java
+++ src/main/java/org/gradle/n3Exe1/entity/EuroYuanConversor.java
@@ -3,15 +3,6 @@ package org.gradle.n3Exe1.entity;
 public class EuroYuanConversor implements CurrencyConverter {
 	
 	private final float EXCHANGE_RATE = 7.80F;
-	private final String CURRENCY = "CNY";
-	
-	public float getEXCHANGE_RATE() {
-		return EXCHANGE_RATE;
-	}
-
-	public String getCURRENCY() {
-		return CURRENCY;
-	}
 	
 	@Override
 	public float conversionFrom(float amount) {
