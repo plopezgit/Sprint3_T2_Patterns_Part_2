b2f7100774c64f2ac20c3c3977f542e5b656e7e8
plopezgit <p.lopez.ch.hr@hotmail.com>
2023-11-13 09:26:58 +0100

    creates main class; reads requirement

---
+++ src/main/java/org/gradle/n2Exe1/client/App.java
@@ -0,0 +1,21 @@
+package org.gradle.n2Exe1.client;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		/*
+			Simula una pasarela de pago que reciba un objeto encapsulador del método de pago a efectuar: 
+			tarjeta de crédito, 
+			Paypal o 
+			debito en cuenta bancaria.
+			
+			La pasarela invocará el pago sin conocer la forma 
+			y devolviendo el control a la clase de origen.
+			
+			La clase que invoca la pasarela de pago será una tienda de zapatos.	
+		*/
+		
+	}
+	
+}
