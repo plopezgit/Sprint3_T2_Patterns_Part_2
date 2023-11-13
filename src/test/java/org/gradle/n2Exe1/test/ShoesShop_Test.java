e72d11c5d590bb413e0cb94926f92afe95680692
plopezgit <p.lopez.ch.hr@hotmail.com>
2023-11-13 15:17:56 +0100

    creates an additional test checkPaymentMethodCallbackIsNotEmpty ();
    adds TestMethodOrder annotation; test passed
    
    Signed-off-by: plopezgit <p.lopez.ch.hr@hotmail.com>

--- src/test/java/org/gradle/n2Exe1/test/ShoesShop_Test.java
+++ src/test/java/org/gradle/n2Exe1/test/ShoesShop_Test.java
@@ -1,18 +1,24 @@
 package org.gradle.n2Exe1.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.gradle.n2Exe1.entity.ShoesShop;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(OrderAnnotation.class)
 public class ShoesShop_Test {
 	
 	private int id;
 	private ShoesShop shop;
 	private float amount;
-	private int method;
-	private String expectedCallbackResponse;
+	private int method; // TDC = 1 Paypal = 2 Bank = 3
+	private String expectedCallbackResponse; 
 	
 	@BeforeEach
 	public void setTestUp () {
@@ -23,7 +29,16 @@ public class ShoesShop_Test {
 		expectedCallbackResponse = "tdc payment done"; 
 	}
 	
-	@DisplayName("Dado un llamado a PaymentGateway, validar el body del callback sea correcto")
+	@DisplayName("Dado un llamado a PaymentGateway, validar que el body del callback no esta vacio")
+	@Order(1)
+	@Test
+	public void checkPaymentMethodCallbackIsNotEmpty () {
+		String response = shop.executePayment(amount, method);
+		assertFalse(response.isEmpty());
+	}
+	
+	@DisplayName("Dado un llamado a PaymentGateway, validar que el body del callback sea correcto")
+	@Order(2)
 	@Test
 	public void checkPaymentMethodCallbackResponse () {
 		String response = shop.executePayment(amount, method);
