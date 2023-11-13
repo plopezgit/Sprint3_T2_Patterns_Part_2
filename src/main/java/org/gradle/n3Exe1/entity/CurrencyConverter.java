6806eff7a99d0857b2584bd55496834ef221e789
plopezgit <p.lopez.ch.hr@hotmail.com>
2023-11-13 15:58:45 +0100

    creates initial entity files

---
+++ src/main/java/org/gradle/n3Exe1/entity/CurrencyConverter.java
@@ -0,0 +1,6 @@
+package org.gradle.n3Exe1.entity;
+
+public class CurrencyConverter {
+	
+	
+}
---
+++ src/main/java/org/gradle/n3Exe1/entity/Vending.java
@@ -0,0 +1,13 @@
+package org.gradle.n3Exe1.entity;
+
+public class Vending {
+	
+	/*
+	Diseña una clase que muestre en pantalla el precio de varios artículos. 
+	Ya que tendrá que mostrarlos también en varios tipos de moneda.
+	Importante:
+		Asegúrate de añadirle como Inyección de Dependencia 
+		una clase Conversor de Moneda que efectúe la corrección 
+		del precio en función del cambio de divisa.
+	*/
+}
