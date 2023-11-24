d5d7a380b0a4f6a07698ab8999c763e18d74979f
plopezgit <p.lopez.ch.hr@hotmail.com>
2023-11-24 11:21:01 +0100

    updates main method demo

--- src/main/java/org/gradle/n2Exe1/client/App.java
+++ src/main/java/org/gradle/n2Exe1/client/App.java
@@ -4,29 +4,10 @@ import org.gradle.n2Exe1.entity.ShoesShop;
 
 public class App {
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		
 		ShoesShop shop = new ShoesShop();
-		System.out.println(shop.executePayment(200, 1));
-		System.out.println(shop.executePayment(200, 2));
-		System.out.println(shop.executePayment(200, 3));
-		
-		// The exercise was validate by unit test
-		
-		/*
-			Simula una **pasarela de pago** que reciba un objeto encapsulador del método de pago a efectuar: 
-			tarjeta de crédito, 
-			Paypal o 
-			debito en cuenta bancaria.
-			
-			**La pasarela invocará el pago sin conocer la forma**
-			
-			y devolviendo el control a la clase de origen.
-			
-			La clase que invoca la pasarela de pago será una tienda de zapatos.	
-		*/
-		
+		System.out.println(shop.executePayment(200, 1)); // TDC = 1 Paypal = 2 Bank = 3	
 	}
 	
 }
